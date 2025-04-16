d304b811173b3012332ca60491f30f9489bf943c
Wool5443 <mihsolodilov2015@gmail.com>
2025-04-16 17:25:58 +0300

    Upd
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- scripts/OCR.sh
+++ scripts/OCR.sh
@@ -4,6 +4,7 @@
 die() {
   exit 1
 }
+
 cleanup() {
   [[ -n $1 ]] && rm -r "$1"
 }
@@ -17,5 +18,7 @@ hyprshot -m region -f scr.png -zs -o $SCR_IMG
 sleep 0.1
 mogrify -modulate 100,0 -resize 400% "$SCR_IMG/scr.png" || die "failed to convert image"
 tesseract -l eng+rus "$SCR_IMG/scr.png" "$SCR_IMG/scr" &>/dev/null || die "failed to extract text"
+sleep 0.1
 wl-copy <"$SCR_IMG/scr.txt" || die "failed to copy text to clipboard"
+sleep 0.1
 exit
