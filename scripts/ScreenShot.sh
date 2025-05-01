07ca829d6c761fff503520668b43a8c603d39b4b
Wool5443 <mihsolodilov2015@gmail.com>
2025-05-01 18:22:10 +0300

    Upd
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- scripts/ScreenShot.sh
+++ scripts/ScreenShot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 screenshotfile=$(hyprshot -zs -m $1 -o ~/Pictures/Screenshots -- echo)
-shouldedit=$(notify-send --app-name=swappy --action="edit=Edit" Screenshot "Screenshot was captured")
+shouldedit=$(notify-send --expire-time=60000 --app-name=swappy --action="edit=Edit" Screenshot "Screenshot was captured")
 
 if [ "$shouldedit" == "edit" ]; then
     swappy -f $screenshotfile -o $screenshotfile
