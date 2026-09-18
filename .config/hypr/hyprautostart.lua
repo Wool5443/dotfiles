cbef163f436fa2e0d3bf268a4da4ae2eea323cdb
Twenty5443 <mihsolodilov2015@gmail.com>
2026-09-18 16:45:12 +0300

    Upd
    Signed-off-by: Twenty5443 <mihsolodilov2015@gmail.com>

--- .config/hypr/hyprautostart.lua
+++ .config/hypr/hyprautostart.lua
@@ -1,12 +1,8 @@
 hl.on("hyprland.start", function()
     -- System
     hl.exec_cmd(
-        "systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP")
-    hl.exec_cmd(
         "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP")
     hl.exec_cmd("systemctl --user start hyprland-session.target")
-    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,ssh,pkcs11")
-    hl.exec_cmd("playerctld daemon")
 
     -- GDrive
     hl.exec_cmd("rclone mount Google: ~/GoogleDrive --vfs-cache-mode full")
@@ -20,6 +16,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("flatpak run app.zen_browser.zen", { workspace = "1" })
     hl.exec_cmd("flatpak run org.telegram.desktop", { workspace = "2 silent" })
     -- hl.exec_cmd("karing")
+    hl.exec_cmd("/opt/Throne/Throne -tray")
     hl.exec_cmd("flatpak run com.spotify.Client", { workspace = "10" })
     hl.exec_cmd("flatpak run com.dec05eba.gpu_screen_recorder gsr-ui")
 end)
